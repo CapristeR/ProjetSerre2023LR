13812934f1d0984bf840913772c71335f01cb341
CapristeR <43319172+CapristeR@users.noreply.github.com>
2023-01-27 09:47:45 +0100

    V0.1 - Première Version du Site "Fonctionnel"

---
+++ credits.php
@@ -0,0 +1,61 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+  <meta charset="UTF-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <meta http-equiv="X-UA-Compatible" content="ie=edge">
+  <link href="https://fonts.googleapis.com/css?family=Raleway:500,800" rel="stylesheet">
+  <link href="stylecredits.css" rel="stylesheet">
+  <title>Document</title>
+</head>
+<body>
+  <div>
+     <div class="wave"></div>
+     <div class="wave"></div>
+     <div class="wave"></div>
+  </div>
+</body>
+ <!-- MENU --> 
+ <div class="area"></div><nav class="main-menu">
+           <ul>
+               <li>
+                   <a href="https://jbfarrow.com">
+                       <i onclick="window.location.href = 'Accueil.php';" class="fa fa-home fa-2x"></i>
+                       <span onclick="window.location.href = 'Accueil.php';" class="nav-text">
+                          Accueil
+                       </span>
+                   </a>
+               <li>
+                  <a href="#">
+                      <i onclick="window.location.href = 'courbes.php';" class="fa fa-cogs fa-2x"></i>
+                       <span onclick="window.location.href = 'courbes.php';" class="nav-text">
+                           Courbes,Graphes
+                       </span>
+                   </a>
+               </li>  
+               <li>
+                   <a href="#">
+                      <i onclick="window.location.href = 'credits.php';" class="fa fa-info fa-2x"></i>
+                       <span onclick="window.location.href = 'credits.php';" class="nav-text">
+                           <strong>Crédits</strong>
+                       </span>
+                   </a>
+               </li>
+           </ul>
+           <ul class="logout">
+               <li>
+                  <a href="#">
+                        <i onclick="window.location.href = 'index.php';" class="fa fa-power-off fa-2x"></i>
+                       <span onclick="window.location.href = 'index.php';" class="nav-text">
+                           Se Déconnecter
+                       </span>
+                   </a>
+               </li>  
+           </ul>
+       </nav>
+ <!--FIN MENU -->
+<h1>666</h1>
+<h2>hey Le site est en construction donc attends un peu oh... Abuse pas frr</h2>
+</body>
+</html>
+
---
+++ pourcourbes.php
@@ -0,0 +1,36 @@
+<!DOCTYPE html>
+<html lang="fr">
+<head>
+    <meta charset="UTF-8">
+    <title>AnyChart PHP template</title>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-base.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-data-adapter.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-exports.min.js"></script>
+    <script src="https://cdn.anychart.com/releases/v8/js/anychart-vml.min.js"></script>
+    <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/css/anychart-ui.min.css" />
+    <link rel="stylesheet" href="https://cdn.anychart.com/releases/v8/fonts/css/anychart.min.css"/>
+    <link rel="stylesheet" href="static/css/style.css"/>
+</head>
+<body>
+<div id="container"></div>
+<script>
+    anychart.data.loadJsonFile("/data.php", function (data) {  // init and draw chart
+        var chart = anychart.bar(data);
+        chart.title("Température / Humidité");
+        chart.container("container");
+        chart.draw();
+
+        // update chart from server every 5 seconds
+        setInterval(function(){
+            // make request to server
+            // to use loadJsonFile function you must include data-adapter.min.js to your page
+            anychart.data.loadJsonFile("/data.php", function (data) {
+                chart.data(data);
+            })
+        }, 5000);
+    });
+</script>
+</body>
+</html>
+
+
